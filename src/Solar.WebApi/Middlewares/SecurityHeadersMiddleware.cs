e71e4af00a5da330fedbcfc0e01366c940a376b3
faahiero <fabriciosilvalp@outlook.com>
2026-08-27 12:05:38 -0300

    feat(security): implementar headers OWASP, resiliencia HTTP e correcoes JWT

--- src/Solar.WebApi/Middlewares/SecurityHeadersMiddleware.cs
+++ src/Solar.WebApi/Middlewares/SecurityHeadersMiddleware.cs
@@ -30,6 +30,12 @@ public class SecurityHeadersMiddleware
             if (!headers.ContainsKey("Permissions-Policy"))
                 headers.Append("Permissions-Policy", "camera=(), microphone=(self \"https://bbb.virtual.ufc.br\"), geolocation=()");
 
+            if (!headers.ContainsKey("Strict-Transport-Security") && context.Request.IsHttps)
+                headers.Append("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+
+            if (!headers.ContainsKey("Content-Security-Policy"))
+                headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https:; frame-src 'self' https://bbb.virtual.ufc.br; connect-src 'self' ws: wss: https:;");
+
             return Task.CompletedTask;
         });
 
